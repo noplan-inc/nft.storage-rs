fccdc6ed04db8e4b2931bb694139021e7dad6b2f
serinuntius <wasab1gj@gmail.com>
2023-11-08 14:12:00 +0900

    Add upload.rs command for file upload

---
+++ nft_storage_cli/src/commands/upload.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use clap::Args;
+use nft_storage_core::NftStorageApi as _;
+
+use crate::{AppContext, Result};
+
+#[derive(Args, Debug)]
+pub struct UploadArgs {
+    #[arg(help = "File to upload path", value_delimiter = ',', required = true)]
+    pub file: Vec<PathBuf>,
+
+    #[clap(short, long)]
+    pub x_agent_did: Option<String>,
+}
+
+impl UploadArgs {
+    pub async fn execute(&self, context: &AppContext) -> Result<()> {
+        context
+            .client
+            .upload(&self.file, self.x_agent_did.as_deref())
+            .await?;
+
+        Ok(())
+    }
+}

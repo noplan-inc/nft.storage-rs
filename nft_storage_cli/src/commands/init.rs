eccfe8594f98474fca612b4d8a65104ca2764be0
serinuntius <wasab1gj@gmail.com>
2023-12-24 23:07:02 +0900

    feat: add api_key to InitArgs and use in execute

--- nft_storage_cli/src/commands/init.rs
+++ nft_storage_cli/src/commands/init.rs
@@ -7,26 +7,39 @@ use crate::{
 };
 
 #[derive(Args, Debug)]
-pub struct InitArgs {}
+pub struct InitArgs {
+    api_key: Option<String>,
+}
 
 impl InitArgs {
+    // pub async fn execute(&self, encryptor: Box<dyn Encryptor + Send + Sync>, _api_key: Option<String>, encrypt_method: String, verbose: bool) -> Result<()> {
     pub async fn execute(&self, context: &AppContext) -> Result<()> {
-        let key = match context.encryptor.generate_key() {
+        let AppContext {
+            client: _,
+            encryptor,
+            encrypt_method,
+            verbose,
+            api_key: _api_key,
+        } = context;
+
+        let key = match encryptor.generate_key() {
             Ok(key) => key,
             Err(e) => {
                 bail!("Failed to generate key: {}", e);
             }
         };
 
+        let api_key = Some(self.api_key.clone().unwrap_or_else(|| _api_key.to_string()));
+
         let config = Config {
-            encrypt_method: Some(context.encrypt_method.clone()),
+            api_key,
+            encrypt_method: Some(encrypt_method.clone()),
             encrypt_private_key: Some(key.clone()),
-            api_key: Some(context.api_key.clone()),
-            verbose: Some(context.verbose),
+            verbose: Some(verbose.to_owned()),
         };
 
         let config_path = write_config(&config, None).await?;
-        println!("Config written to {}", config_path.display());
+        println!("Config written to {:?}", config_path);
 
         Ok(())
     }

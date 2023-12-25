c731e20629ede20844e7497f5ca6ef6159b53366
serinuntius <wasab1gj@gmail.com>
2023-12-25 22:06:47 +0900

    Refactor upload examples to remove unnecessary lines

--- nft_storage_core/examples/upload_encrypted.rs
+++ nft_storage_core/examples/upload_encrypted.rs
@@ -10,7 +10,7 @@ async fn main() {
     // If pass none, use NFT_STORAGE_API_KEY envrioment variable.
     let api_key = Some("<FILL ME NFT_STORAGE_API_KEY>".to_string());
     let client = NftStorageCore::try_new(api_key, encryptor).unwrap();
-    
+
     let path = vec![PathBuf::from("test.txt")];
     let resp = client.upload_encrypted(&path, None).await.unwrap();
     println!("{}", serde_json::to_string_pretty(&resp).unwrap());

f267ab7a3173d7a3a8054ff204dda4ea2d51faf8
serinuntius <wasab1gj@gmail.com>
2023-12-25 21:56:13 +0900

    Add upload example code for NFT storage core

---
+++ nft_storage_core/examples/upload.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+
+use nft_storage_core::{
+    encryptor::plugins::aes::AesEncryptor,
+    NftStorageApi, NftStorageCore,
+};
+
+#[tokio::main]
+async fn main() {
+    let aes = AesEncryptor::generate_key().unwrap();
+
+    let encryptor = Box::new(aes);
+    let client = NftStorageCore::try_new(None, encryptor).unwrap();
+
+    let path = vec![PathBuf::from("test.txt")];
+    let resp = client.upload(&path, None).await.unwrap();
+    println!("{}", serde_json::to_string_pretty(&resp).unwrap());
+}

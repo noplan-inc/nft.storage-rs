114d4db97b802492466afe5b277ba2efec8ff79f
serinuntius <wasab1gj@gmail.com>
2023-12-25 22:05:50 +0900

    Add API key support for download and upload examples

--- nft_storage_core/examples/download.rs
+++ nft_storage_core/examples/download.rs
@@ -7,7 +7,10 @@ async fn main() {
     let aes = AesEncryptor::generate_key().unwrap();
 
     let encryptor = Box::new(aes);
-    let client = NftStorageCore::try_new(None, encryptor).unwrap();
+
+    // If pass none, use NFT_STORAGE_API_KEY envrioment variable.
+    let api_key = Some("<FILL ME NFT_STORAGE_API_KEY>".to_string());
+    let client = NftStorageCore::try_new(api_key, encryptor).unwrap();
 
     // CID only works with what nft.storage manages for now.
     // In the future, any IPFS CID will work.
--- nft_storage_core/examples/upload_encrypted.rs
+++ nft_storage_core/examples/upload_encrypted.rs
@@ -7,8 +7,10 @@ async fn main() {
     let aes = AesEncryptor::generate_key().unwrap();
 
     let encryptor = Box::new(aes);
-    let client = NftStorageCore::try_new(None, encryptor).unwrap();
-
+    // If pass none, use NFT_STORAGE_API_KEY envrioment variable.
+    let api_key = Some("<FILL ME NFT_STORAGE_API_KEY>".to_string());
+    let client = NftStorageCore::try_new(api_key, encryptor).unwrap();
+    
     let path = vec![PathBuf::from("test.txt")];
     let resp = client.upload_encrypted(&path, None).await.unwrap();
     println!("{}", serde_json::to_string_pretty(&resp).unwrap());
